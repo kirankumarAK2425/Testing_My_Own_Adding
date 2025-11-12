48a279e5653144476e19a84d8428cda889979f0b
kirankumarak2425 <kirankumarb141@gmail.com>
2025-11-12 17:01:18 +0530

    test

--- src/test/java/Testing/NewTest.java
+++ src/test/java/Testing/NewTest.java
@@ -2,9 +2,18 @@ package Testing;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 
 public class NewTest {
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	@BeforeMethod
+	  public void beforeMethod() {
+	  }
+	
   @Test
   public void f() {
 	  
@@ -12,9 +21,7 @@ public class NewTest {
 	  
 	  
   }
-  @BeforeMethod
-  public void beforeMethod() {
-  }
+  
 
   @AfterMethod
   public void afterMethod() {
